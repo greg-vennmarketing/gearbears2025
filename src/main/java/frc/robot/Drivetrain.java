2f71b4c5ffa18efe57d559b8fc7294460abc958a
GearBears9480Code <gearbears9480code@gmail.com>
2025-02-04 16:11:43 -0500

    resolve errors

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -1,8 +1,8 @@
-public // Copyright (c) FIRST and other WPILib contributors.
+ // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package edu.wpi.first.wpilibj.examples.swervebot;
+package frc.robot;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -83,6 +83,5 @@ public class Drivetrain {
           m_backRight.getPosition()
         });
   }
-} {
-    
+
 }
