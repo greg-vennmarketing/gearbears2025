18936ca52bcfbc6a0a55ad91a26c1363325d017b
GearBearsCode <gearbears9480code@gmail.com>
2025-10-01 13:50:40 -0400

    auto straight back, elevator to L4 confirrmed

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -203,15 +203,15 @@ public Command driveFieldOriented(Supplier<ChassisSpeeds> velocity)
   /**
    * Get the path follower with events.
    *
-   * @param auto67 PathPlanner path name.
-   *  * @return {@link AutoBuilder#followPath(test67)} path command.
+   * @param straight_back_20251001_v1 PathPlanner path name.
+   *  * @return {@link AutoBuilder#followPath(straight_back_20251001_v1_path)} path command.
 
    * 
    */
-  public Command getAutonomousCommand(String auto67)
+  public Command getAutonomousCommand(String straight_back_20251001_v1)
   {
     // Create a path following command using AutoBuilder. This will also trigger event markers.
-    return new PathPlannerAuto(auto67);
+    return new PathPlannerAuto(straight_back_20251001_v1);
   }
 
 }
